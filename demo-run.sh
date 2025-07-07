1779cc1009c54e07397caa09a9eb1cc486b528f4
LungWai <42887219+LungWai@users.noreply.github.com>
2025-07-08 02:27:06 +0800

    Create demo-run.sh

---
+++ demo-run.sh
@@ -0,0 +1,3 @@
+bash <(curl -s https://raw.githubusercontent.com/LungWai/vm-tools/main/cc-install.sh)
+
+bash <(curl -s https://raw.githubusercontent.com/LungWai/vm-tools/main/vscode-extensions-install.sh)
