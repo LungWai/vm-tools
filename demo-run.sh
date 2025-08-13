b75664a86e27341a4be6957fbea506c38eb81a01
LungWai <42887219+LungWai@users.noreply.github.com>
2025-08-13 20:37:46 +0800

    Update demo-run.sh

--- demo-run.sh
+++ demo-run.sh
@@ -1,3 +1,5 @@
 bash <(curl -s https://raw.githubusercontent.com/LungWai/vm-tools/main/cc-install.sh)
 
-bash <(curl -s https://raw.githubusercontent.com/LungWai/vm-tools/main/ext-install.sh)
+
+curl -fsSL -o /tmp/setup-data-disk.sh https://raw.githubusercontent.com/LungWai/vm-tools/main/setup-data-disk.sh
+sudo bash /tmp/setup-data-disk.sh
